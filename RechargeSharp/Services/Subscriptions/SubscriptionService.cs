0761926bcd3bd6df63e5806eacafb9ee8ab30b4a
Matt Jensen <mj@vuffeli.dk>
2019-07-22 11:23:15 +0200

    Created a method to fetch all subscriptions with specific parameters instead of having to implement the paging logic in the application using the package.

--- RechargeSharp/Services/Subscriptions/SubscriptionService.cs
+++ RechargeSharp/Services/Subscriptions/SubscriptionService.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using RechargeSharp.Entities.Shared;
@@ -26,7 +27,7 @@ namespace RechargeSharp.Services.Subscriptions
                 await response.Content.ReadAsStringAsync());
         }
 
-        public async Task<SubscriptionListResponse> GetSubscriptionsAsync(int page = 1, int limit = 50, string customerId = null, string addressId = null, string status = null, string shopifyCustomerId = null, string shopifyVariantId = null, DateTime? createdAtMin = null, DateTime? createAtMax = null, DateTime? updatedAtMin = null, DateTime? updatedAtMax = null)
+        public Task<SubscriptionListResponse> GetSubscriptionsAsync(int page = 1, int limit = 50, string customerId = null, string addressId = null, string status = null, string shopifyCustomerId = null, string shopifyVariantId = null, DateTime? createdAtMin = null, DateTime? createAtMax = null, DateTime? updatedAtMin = null, DateTime? updatedAtMax = null)
         {
             var queryParams = $"page={page}&limit={limit}";
             queryParams += customerId != null ? $"&customer_id={customerId}" : "";
@@ -40,7 +41,39 @@ namespace RechargeSharp.Services.Subscriptions
             queryParams += updatedAtMax != null ? $"&updated_at_max={updatedAtMax?.ToString("s")}" : "";
 
 
-            return await GetSubscriptionsAsync(queryParams);
+            return GetSubscriptionsAsync(queryParams);
+        }
+
+        public Task<SubscriptionListResponse> GetAllSubscriptionsWithParamsAsync(string customerId = null, string addressId = null, string status = null, string shopifyCustomerId = null, string shopifyVariantId = null, DateTime? createdAtMin = null, DateTime? createAtMax = null, DateTime? updatedAtMin = null, DateTime? updatedAtMax = null)
+        {
+            var queryParams = "";
+            queryParams += customerId != null ? $"&customer_id={customerId}" : "";
+            queryParams += addressId != null ? $"&address_id={addressId}" : "";
+            queryParams += status != null ? $"&status={status}" : "";
+            queryParams += shopifyCustomerId != null ? $"&shopify_customer_id={shopifyCustomerId}" : "";
+            queryParams += shopifyVariantId != null ? $"&shopify_variant_id={shopifyVariantId}" : "";
+            queryParams += createdAtMin != null ? $"&created_at_min={createdAtMin?.ToString("s")}" : "";
+            queryParams += createAtMax != null ? $"&created_at_max={createAtMax?.ToString("s")}" : "";
+            queryParams += updatedAtMin != null ? $"&updated_at_min={updatedAtMin?.ToString("s")}" : "";
+            queryParams += updatedAtMax != null ? $"&updated_at_max={updatedAtMax?.ToString("s")}" : "";
+            return GetSubscriptionsRecAsync(queryParams, 1, new SubscriptionListResponse() { Subscriptions = new List<Subscription>() });
+        }
+
+        private async Task<SubscriptionListResponse> GetSubscriptionsRecAsync(string queryParams, int page, SubscriptionListResponse accumulator)
+        {
+            var response = await GetAsync($"/Subscriptions?page={page}&limit=250{queryParams}");
+            var result = JsonConvert.DeserializeObject<SubscriptionListResponse>(
+                await response.Content.ReadAsStringAsync());
+            if (result.Subscriptions.Count == 0)
+            {
+                return accumulator;
+            }
+            else
+            {
+                page++;
+                accumulator.Subscriptions.AddRange(result.Subscriptions);
+                return await GetSubscriptionsRecAsync(queryParams, page, accumulator);
+            }
         }
 
         public async Task<CountResponse> CountSubscriptionsAsync()
@@ -58,7 +91,7 @@ namespace RechargeSharp.Services.Subscriptions
         }
         public async Task<SubscriptionResponse> ChangeNextChargeDateAsync(string id, DateTimeOffset date)
         {
-            var response = await PostAsync($"/Subscriptions/{id}/set_next_charge_date", JsonConvert.SerializeObject(new ChangeNextChargeDateRequest {Date = date.ToString("yyyy-MM-dd")}));
+            var response = await PostAsync($"/Subscriptions/{id}/set_next_charge_date", JsonConvert.SerializeObject(new ChangeNextChargeDateRequest { Date = date.ToString("yyyy-MM-dd") }));
             return JsonConvert.DeserializeObject<SubscriptionResponse>(
                 await response.Content.ReadAsStringAsync());
         }
