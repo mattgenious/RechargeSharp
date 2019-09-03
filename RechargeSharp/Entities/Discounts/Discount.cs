62e1f62bd7a01897c810dac0eca5db8c6fe7a976
Matt Luccas Phaure Jensen <matt.l.p.jensen@gmail.com>
2019-09-03 11:33:07 +0200

    fixed some entity bugs for discounts

--- RechargeSharp/Entities/Discounts/Discount.cs
+++ RechargeSharp/Entities/Discounts/Discount.cs
@@ -15,16 +15,16 @@ namespace RechargeSharp.Entities.Discounts
         public long Value { get; set; }
 
         [JsonProperty("ends_at")]
-        public DateTimeOffset EndsAt { get; set; }
+        public DateTimeOffset? EndsAt { get; set; }
 
         [JsonProperty("starts_at")]
-        public DateTimeOffset StartsAt { get; set; }
+        public DateTimeOffset? StartsAt { get; set; }
 
         [JsonProperty("status")]
         public string Status { get; set; }
 
         [JsonProperty("usage_limit")]
-        public long UsageLimit { get; set; }
+        public long? UsageLimit { get; set; }
 
         [JsonProperty("applies_to_id")]
         public long? AppliesToId { get; set; }
@@ -45,7 +45,7 @@ namespace RechargeSharp.Entities.Discounts
         public string Duration { get; set; }
 
         [JsonProperty("duration_usage_limit")]
-        public long DurationUsageLimit { get; set; }
+        public long? DurationUsageLimit { get; set; }
 
         [JsonProperty("applies_to_product_type")]
         public string AppliesToProductType { get; set; }
