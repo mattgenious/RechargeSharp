9dce4ed37076c8d75545882e3b3e18a611f6a32a
Matt Luccas Phaure Minet <matt.l.p.jensen@gmail.com>
2019-09-07 11:44:15 +0200

    applied discount variables nullable

--- RechargeSharp/Entities/Checkouts/CheckoutAppliedDiscount.cs
+++ RechargeSharp/Entities/Checkouts/CheckoutAppliedDiscount.cs
@@ -7,19 +7,19 @@ namespace RechargeSharp.Entities.Checkouts
 {
     public class CheckoutAppliedDiscount
     {
-        [JsonProperty("amount")]
-        public long Amount { get; set; }
+        [JsonProperty("amount", NullValueHandling = NullValueHandling.Ignore)]
+        public decimal? Amount { get; set; }
 
-        [JsonProperty("value")]
-        public long Value { get; set; }
+        [JsonProperty("value", NullValueHandling = NullValueHandling.Ignore)]
+        public decimal? Value { get; set; }
 
-        [JsonProperty("value_type")]
+        [JsonProperty("value_type", NullValueHandling = NullValueHandling.Ignore)]
         public string ValueType { get; set; }
 
-        [JsonProperty("applicable")]
-        public bool Applicable { get; set; }
+        [JsonProperty("applicable", NullValueHandling = NullValueHandling.Ignore)]
+        public bool? Applicable { get; set; }
 
-        [JsonProperty("non_applicable_reason")]
+        [JsonProperty("non_applicable_reason", NullValueHandling = NullValueHandling.Ignore)]
         public object NonApplicableReason { get; set; }
     }
 }
