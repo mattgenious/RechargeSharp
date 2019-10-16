b16a07d10a51ef40d008d50fd9e5f8afb4b2f354
Matt Luccas Phaure Minet <matt.l.p.jensen@gmail.com>
2019-10-16 09:41:49 +0200

    Fixed UpdateSubscriptionRequest, the datastructure was wrong.

--- RechargeSharp/Entities/Subscriptions/UpdateSubscriptionRequest.cs
+++ RechargeSharp/Entities/Subscriptions/UpdateSubscriptionRequest.cs
@@ -7,11 +7,14 @@ namespace RechargeSharp.Entities.Subscriptions
 {
     public class UpdateSubscriptionRequest
     {
-        [JsonProperty("address_id", NullValueHandling = NullValueHandling.Ignore)]
-        public long? AddressId { get; set; }
+        [JsonProperty("order_interval_unit")]
+        public string OrderIntervalUnit { get; set; }
 
-        [JsonProperty("next_charge_scheduled_at", NullValueHandling = NullValueHandling.Ignore)]
-        public DateTime? NextChargeScheduledAt { get; set; }
+        [JsonProperty("order_interval_frequency")]
+        public long OrderIntervalFrequency { get; set; }
+
+        [JsonProperty("charge_interval_frequency")]
+        public long ChargeIntervalFrequency { get; set; }
 
         [JsonProperty("product_title", NullValueHandling = NullValueHandling.Ignore)]
         public string ProductTitle { get; set; }
@@ -28,11 +31,11 @@ namespace RechargeSharp.Entities.Subscriptions
         [JsonProperty("shopify_variant_id", NullValueHandling = NullValueHandling.Ignore)]
         public long? ShopifyVariantId { get; set; }
 
-        [JsonProperty("sku_override", NullValueHandling = NullValueHandling.Ignore)]
-        public bool? SkuOverride { get; set; }
+        [JsonProperty("order_day_of_week", NullValueHandling = NullValueHandling.Ignore)]
+        public long? OrderDayOfWeek { get; set; }
 
-        [JsonProperty("sku", NullValueHandling = NullValueHandling.Ignore)]
-        public string Sku { get; set; }
+        [JsonProperty("order_day_of_month", NullValueHandling = NullValueHandling.Ignore)]
+        public long? OrderDayOfMonth { get; set; }
 
         [JsonProperty("expire_after_specific_number_of_charges", NullValueHandling = NullValueHandling.Ignore)]
         public long? ExpireAfterSpecificNumberOfCharges { get; set; }
@@ -40,19 +43,10 @@ namespace RechargeSharp.Entities.Subscriptions
         [JsonProperty("properties", NullValueHandling = NullValueHandling.Ignore)]
         public List<Property> Properties { get; set; }
 
-        [JsonProperty("order_interval_unit")]
-        public string OrderIntervalUnit { get; set; }
+        [JsonProperty("sku", NullValueHandling = NullValueHandling.Ignore)]
+        public string Sku { get; set; }
 
-        [JsonProperty("order_interval_frequency")]
-        public long OrderIntervalFrequency { get; set; }
-
-        [JsonProperty("charge_interval_frequency")]
-        public long ChargeIntervalFrequency { get; set; }
-
-        [JsonProperty("order_day_of_week", NullValueHandling = NullValueHandling.Ignore)]
-        public long? OrderDayOfWeek { get; set; }
-
-        [JsonProperty("order_day_of_month", NullValueHandling = NullValueHandling.Ignore)]
-        public long? OrderDayOfMonth { get; set; }
+        [JsonProperty("sku_override", NullValueHandling = NullValueHandling.Ignore)]
+        public bool? SkuOverride { get; set; }
     }
 }
