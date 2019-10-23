77ebdd490560ad2f5eb98e28e7e14c8535d1fc06
Matt Luccas Phaure Jensen <matt.l.p.jensen@gmail.com>
2019-10-23 13:31:32 +0200

    Improved performance of get addresses and get all addresses as well as implementing get all addresses for specific customer id.

--- RechargeSharp/Services/Addresses/AddressService.cs
+++ RechargeSharp/Services/Addresses/AddressService.cs
@@ -21,6 +21,13 @@ namespace RechargeSharp.Services.Addresses
                 await response.Content.ReadAsStringAsync().ConfigureAwait(false)).Address;
         }
 
+        public async Task<IEnumerable<Address>> GetAllAddressesForCustomerAsync(string customerId)
+        {
+            var response = await GetAsync($"/customers/{customerId}/addresses").ConfigureAwait(false);
+            return JsonConvert.DeserializeObject<AddressListResponse>(
+                await response.Content.ReadAsStringAsync().ConfigureAwait(false)).Addresses;
+        }
+
         private async Task<IEnumerable<Address>> GetAddressesAsync(string queryParams)
         {
             var response = await GetAsync($"/addresses?{queryParams}").ConfigureAwait(false);
@@ -37,6 +44,64 @@ namespace RechargeSharp.Services.Addresses
             queryParams += updatedAtMax != null ? $"&updated_at_max={updatedAtMax?.ToString("s")}" : "";
 
             return GetAddressesAsync(queryParams);
+        }
+
+        public Task<IEnumerable<Address>> GetAllAddressesWithParamsAsync(string email = null, string status = null, string shopifyCustomerId = null, DateTime? createdAtMin = null, DateTime? createAtMax = null, DateTime? updatedAtMin = null, DateTime? updatedAtMax = null, string hash = null)
+        {
+            var queryParams = "";
+            queryParams += email != null ? $"&email={email}" : "";
+            queryParams += status != null ? $"&status={status}" : "";
+            queryParams += shopifyCustomerId != null ? $"&shopify_customer_id={shopifyCustomerId}" : "";
+            queryParams += createdAtMin != null ? $"&created_at_min={createdAtMin?.ToString("s")}" : "";
+            queryParams += createAtMax != null ? $"&created_at_max={createAtMax?.ToString("s")}" : "";
+            queryParams += updatedAtMin != null ? $"&updated_at_min={updatedAtMin?.ToString("s")}" : "";
+            queryParams += updatedAtMax != null ? $"&updated_at_max={updatedAtMax?.ToString("s")}" : "";
+            queryParams += hash != null ? $"&hash={hash}" : "";
+
+            return GetAllAddressesAsync(queryParams);
+        }
+
+        private async Task<IEnumerable<Address>> GetAllAddressesAsync(string queryParams)
+        {
+            var count = await CountAddressesAsync(queryParams);
+
+            var taskList = new List<Task<IEnumerable<Address>>>();
+
+            var pages = Math.Ceiling(Convert.ToDouble(count) / 250d);
+
+            for (int i = 1; i <= Convert.ToInt32(pages); i++)
+            {
+                taskList.Add(GetAddressesAsync($"page={i}&limit=250" + queryParams));
+            }
+
+            var computed = await Task.WhenAll(taskList);
+
+            var result = new List<Address>();
+
+            foreach (var addresses in computed)
+            {
+                result.AddRange(addresses);
+            }
+
+            return result;
+        }
+
+        public async Task<long> CountAddressesAsync(string status = null, DateTime? createdAtMin = null, DateTime? createAtMax = null, DateTime? updatedAtMin = null, DateTime? updatedAtMax = null)
+        {
+            var queryParams = "";
+            queryParams += status != null ? $"&status={status}" : "";
+            queryParams += createdAtMin != null ? $"&created_at_min={createdAtMin?.ToString("s")}" : "";
+            queryParams += createAtMax != null ? $"&created_at_max={createAtMax?.ToString("s")}" : "";
+            queryParams += updatedAtMin != null ? $"&updated_at_min={updatedAtMin?.ToString("s")}" : "";
+            queryParams += updatedAtMax != null ? $"&updated_at_max={updatedAtMax?.ToString("s")}" : "";
+
+            return await CountAddressesAsync(queryParams);
+        }
+        private async Task<long> CountAddressesAsync(string queryParams)
+        {
+            var response = await GetAsync($"/addresses/count?{queryParams}").ConfigureAwait(false);
+            return JsonConvert.DeserializeObject<CountResponse>(
+                await response.Content.ReadAsStringAsync().ConfigureAwait(false)).Count;
         }
 
         public async Task<Address> CreateAddressAsync(CreateAddressRequest createAddressRequest, string customerId)
