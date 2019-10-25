695be41b5d9e0270a05da67ad8cc88f393c13e1f
Matt Luccas Phaure Jensen <matt.l.p.jensen@gmail.com>
2019-10-25 14:43:10 +0200

    Added cancellation_reason_comments to CancelSubscriptionRequest.

--- RechargeSharp/Entities/Subscriptions/CancelSubscriptionRequest.cs
+++ RechargeSharp/Entities/Subscriptions/CancelSubscriptionRequest.cs
@@ -9,6 +9,8 @@ namespace RechargeSharp.Entities.Subscriptions
     {
         [JsonProperty("cancellation_reason")]
         public string CancellationReason { get; set; }
+        [JsonProperty("cancellation_reason_comments", NullValueHandling = NullValueHandling.Ignore)]
+        public string CancellationReasonComments { get; set; }
 
         [JsonProperty("send_email", NullValueHandling = NullValueHandling.Ignore)]
         public bool? SendEmail { get; set; }
