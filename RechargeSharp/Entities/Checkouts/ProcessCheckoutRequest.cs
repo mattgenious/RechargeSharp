c2e43b6af870067124129d2f49b3c11e0094fb02
Matt Luccas Phaure Minet <matt.l.p.jensen@gmail.com>
2021-05-13 22:02:31 +0200

    Included authorization token and free as extra optional parameters in process checkout request.

--- RechargeSharp/Entities/Checkouts/ProcessCheckoutRequest.cs
+++ RechargeSharp/Entities/Checkouts/ProcessCheckoutRequest.cs
@@ -48,5 +48,12 @@ namespace RechargeSharp.Entities.Checkouts
         [Required]
         [JsonProperty("payment_token")]
         public string PaymentToken { get; set; }
+
+        [Required]
+        [JsonProperty("authorization_token", NullValueHandling = NullValueHandling.Ignore)]
+        public string AuthorizationToken { get; set; }
+        
+        [JsonProperty("free", NullValueHandling = NullValueHandling.Ignore)]
+        public bool Free { get; set; }
     }
 }
