8d704b0c245b150d0a0187633078af998e7fb045
Matt Luccas Phaure Minet <matt.l.p.jensen@gmail.com>
2021-12-09 13:57:23 +0100

    Fixed inability to define a key on a derived type for LineItem by reimplementing it instead of inheriting

--- RechargeSharp.Entities.EntityFrameworkCore/Entities/Shared/LineItem.cs
+++ RechargeSharp.Entities.EntityFrameworkCore/Entities/Shared/LineItem.cs
@@ -7,7 +7,7 @@ using RechargeSharp.Entities.Shared;
 
 namespace RechargeSharp.Entities.EntityFrameworkCore.Entities.Shared
 {
-    public class LineItem : RechargeSharp.Entities.Shared.LineItem
+    public class LineItem
     {
         [JsonIgnore]
         public string? Id { get; set; }
@@ -23,10 +23,78 @@ namespace RechargeSharp.Entities.EntityFrameworkCore.Entities.Shared
 
         [JsonProperty("images")]
         [NotMapped]
-        public new Images? Images { get; set; }
+        public Images? Images { get; set; }
 
         [JsonProperty("properties")]
         [NotMapped]
-        public new ICollection<Property>? Properties { get; set; }
+        public ICollection<Property>? Properties { get; set; }
+
+        [JsonProperty("grams")]
+        public long Grams { get; set; }
+
+        [JsonProperty("price")]
+        public string? Price { get; set; }
+
+        [JsonProperty("quantity")]
+        public long Quantity { get; set; }
+
+        [JsonProperty("shopify_product_id")]
+        public long ShopifyProductId { get; set; }
+
+        [JsonProperty("shopify_variant_id")]
+        public string? ShopifyVariantId { get; set; }
+
+        [JsonProperty("sku")]
+        public string? Sku { get; set; }
+
+        [JsonProperty("subscription_id")]
+        public long SubscriptionId { get; set; }
+
+        [JsonProperty("title")]
+        public string? Title { get; set; }
+
+        [JsonProperty("variant_title")]
+        public string? VariantTitle { get; set; }
+
+        public bool Equals(LineItem other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return Grams == other.Grams && Equals(Images, other.Images) && Price == other.Price && Quantity == other.Quantity && ShopifyProductId == other.ShopifyProductId && ShopifyVariantId == other.ShopifyVariantId && Sku == other.Sku && SubscriptionId == other.SubscriptionId && Title == other.Title && VariantTitle == other.VariantTitle;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((LineItem)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            HashCode hash = new HashCode();
+            hash.Add(Grams);
+            hash.Add(Images);
+            hash.Add(Price);
+            hash.Add(Quantity);
+            hash.Add(ShopifyProductId);
+            hash.Add(ShopifyVariantId);
+            hash.Add(Sku);
+            hash.Add(SubscriptionId);
+            hash.Add(Title);
+            hash.Add(VariantTitle);
+            return hash.ToHashCode();
+        }
+
+        public static bool operator ==(LineItem left, LineItem right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(LineItem left, LineItem right)
+        {
+            return !Equals(left, right);
+        }
     }
 }
