293cbaa1c812b078ffd55450b55caf7340912a43
Jesper Nysteen <code@jnysteen.com>
2022-03-04 11:44:50 +0100

    change list of supported JSON types to whitelist

--- RechargeSharp.v2021-11/Utilities/Queries/ObjectToQueryStringSerializer.cs
+++ RechargeSharp.v2021-11/Utilities/Queries/ObjectToQueryStringSerializer.cs
@@ -23,13 +23,16 @@ public static class ObjectToQueryStringSerializer
         {
             switch (jsonProperty.Value.ValueKind)
             {
-                case JsonValueKind.Undefined:
-                case JsonValueKind.Object:
-                case JsonValueKind.Array:
+                case JsonValueKind.Number:
+                case JsonValueKind.String:
+                case JsonValueKind.True:
+                case JsonValueKind.False:
+                case JsonValueKind.Null:
+                    queryString = queryString.Add(jsonProperty.Name, jsonProperty.Value.ToString());
+                    break;
+                default:
                     throw new ArgumentOutOfRangeException(nameof(jsonProperty.Value.ValueKind), "Serialization of this JSON type into query parameters has not been implemented");
             }
-
-            queryString = queryString.Add(jsonProperty.Name, jsonProperty.Value.ToString());
         }
 
         return queryString;
