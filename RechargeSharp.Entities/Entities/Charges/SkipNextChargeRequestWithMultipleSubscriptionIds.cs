12a18118718b54b74def9358390dfa65e9d6ef6a
Matt Luccas Phaure Minet <matt.l.p.jensen@gmail.com>
2022-06-02 11:42:00 +0200

    minor refactor

---
+++ RechargeSharp.Entities/Entities/Charges/SkipNextChargeRequestWithMultipleSubscriptionIds.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Newtonsoft.Json;
+
+namespace RechargeSharp.Entities.Charges
+{
+    public class SkipNextChargeRequestWithMultipleSubscriptionIds : IEquatable<SkipNextChargeRequestWithMultipleSubscriptionIds>, ISkipNextChargeRequest
+    {
+        public bool Equals(SkipNextChargeRequestWithMultipleSubscriptionIds? other)
+        {
+            if (other is null) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return SubscriptionId == other.SubscriptionId;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is null) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((SkipNextChargeRequestWithMultipleSubscriptionIds)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return (SubscriptionId != null ? SubscriptionId.GetHashCode() : 0);
+        }
+
+        public static bool operator ==(SkipNextChargeRequestWithMultipleSubscriptionIds left, SkipNextChargeRequestWithMultipleSubscriptionIds right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(SkipNextChargeRequestWithMultipleSubscriptionIds left, SkipNextChargeRequestWithMultipleSubscriptionIds right)
+        {
+            return !Equals(left, right);
+        }
+
+        [Required]
+        [JsonProperty("subscription_ids")]
+        public string[]? SubscriptionId { get; set; }
+    }
+}
--- RechargeSharp.Entities/Entities/Charges/SkipNextChargeRequestWithSingleSubscriptionId.cs
+++ RechargeSharp.Entities/Entities/Charges/SkipNextChargeRequestWithSingleSubscriptionId.cs
@@ -39,45 +39,4 @@ namespace RechargeSharp.Entities.Charges
         [JsonProperty("subscription_id")]
         public string? SubscriptionId { get; set; }
     }
-    public class SkipNextChargeRequestWithMultipleSubscriptionIds : IEquatable<SkipNextChargeRequestWithMultipleSubscriptionIds>, ISkipNextChargeRequest
-    {
-        public bool Equals(SkipNextChargeRequestWithMultipleSubscriptionIds? other)
-        {
-            if (other is null) return false;
-            if (ReferenceEquals(this, other)) return true;
-            return SubscriptionId == other.SubscriptionId;
-        }
-
-        public override bool Equals(object? obj)
-        {
-            if (obj is null) return false;
-            if (ReferenceEquals(this, obj)) return true;
-            if (obj.GetType() != this.GetType()) return false;
-            return Equals((SkipNextChargeRequestWithMultipleSubscriptionIds)obj);
-        }
-
-        public override int GetHashCode()
-        {
-            return (SubscriptionId != null ? SubscriptionId.GetHashCode() : 0);
-        }
-
-        public static bool operator ==(SkipNextChargeRequestWithMultipleSubscriptionIds left, SkipNextChargeRequestWithMultipleSubscriptionIds right)
-        {
-            return Equals(left, right);
-        }
-
-        public static bool operator !=(SkipNextChargeRequestWithMultipleSubscriptionIds left, SkipNextChargeRequestWithMultipleSubscriptionIds right)
-        {
-            return !Equals(left, right);
-        }
-
-        [Required]
-        [JsonProperty("subscription_ids")]
-        public string[]? SubscriptionId { get; set; }
-    }
-
-    public interface ISkipNextChargeRequest
-    {
-        
-    }
 }
