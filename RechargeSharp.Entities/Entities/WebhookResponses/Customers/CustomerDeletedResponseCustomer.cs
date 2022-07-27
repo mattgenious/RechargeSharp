795fdedafedc64ccebd6077d0ae96090ddcc30f6
Matt Luccas Phaure Minet <matt.l.p.jensen@gmail.com>
2022-07-27 10:24:52 +0200

    added a response entity for customer deleted

---
+++ RechargeSharp.Entities/Entities/WebhookResponses/Customers/CustomerDeletedResponseCustomer.cs
@@ -0,0 +1,11 @@
+﻿using Newtonsoft.Json;
+
+namespace RechargeSharp.Entities.WebhookResponses.Customers
+{
+    public class CustomerDeletedResponseCustomer
+    {
+        // is type object because we don't actually know if its a string or a long because the example from Recharge's slack channel shows a string, but the customer id is a long.
+        [JsonProperty("id")]
+        public long Id { get; set; }
+    }
+}
