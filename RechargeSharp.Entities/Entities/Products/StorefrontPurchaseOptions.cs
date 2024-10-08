1128025fac1234d8908d1e37f40604e1f1b1d971
Matt Jensen <matt.jensen@lego.com>
2024-10-08 23:07:37 +0700

    Update project files and add macOS specific ignore; increment version to 12.2.3 and add missing storefront purchase option

--- RechargeSharp.Entities/Entities/Products/StorefrontPurchaseOptions.cs
+++ RechargeSharp.Entities/Entities/Products/StorefrontPurchaseOptions.cs
@@ -7,6 +7,8 @@ namespace RechargeSharp.Entities.Products
     [JsonConverter(typeof(StringEnumConverter))]
     public enum StorefrontPurchaseOptions
     {
+        [EnumMember(Value = "onetime_only")]
+        OnetimeOnly,
         [EnumMember(Value = "subscription_only")]
         SubscriptionOnly,
         [EnumMember(Value = "subscription_and_onetime")]
